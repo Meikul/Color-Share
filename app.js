714f48414536b312bce10b174b073ea17d141d4d
Atomic Jolt <atomicjolt@Atomics-MacBook-Pro-2.local>
2016-12-17 22:35:22 -0700

    inital commit

---
+++ app.js
@@ -0,0 +1,30 @@
+(function(){
+  var config = {
+    apiKey: "AIzaSyDtPZdkvokETEdPm1X-x2z2AZKm8z0CM78",
+    authDomain: "color-share.firebaseapp.com",
+    databaseURL: "https://color-share.firebaseio.com",
+    storageBucket: "color-share.appspot.com",
+    messagingSenderId: "30330701234"
+  };
+  firebase.initializeApp(config);
+  const dbRef = firebase.database().ref();
+
+  window.onclick = function(){
+    var num = [];
+    for (var i=0; i<3; i++){
+      num[i] = Math.floor(Math.random() * 255);
+      console.log(num[i]);
+    }
+    dbRef.set({
+      red: num[0],
+      green: num[1],
+      blue: num[2]
+    });
+  }
+
+  dbRef.on('value', snap => {
+    let val = snap.val();
+    document.body.style.backgroundColor = "rgb("+val.red+","+val.blue+","+val.green+")";
+  });
+  console.log('good?');
+}());
